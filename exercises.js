3e220f1e3725d66eeb222d6e641c04f482f53439
Ben Tilghman <ben.tilghman@gmail.com>
2019-03-26 17:25:07 -0400

    DONE!

--- exercises.js
+++ exercises.js
@@ -6,7 +6,7 @@ function max(arr) {
 
   while (i < arr.length) {
     if (arr[i] > newNum) {
-      newNum = arr[i]; 
+      newNum = arr[i];
     }
     i++;
   }
@@ -26,12 +26,12 @@ function min(arr) {
   return newNum;
 }
 
-function avarage(arr){
+function avarage(arr) {
   let count = 0;
-  arr.forEach(function(num){
+  arr.forEach(function (num) {
     return count += num;
   });
-  return count/arr.length;
+  return count / arr.length;
 }
 
 
@@ -50,9 +50,9 @@ function goodbye() {
 }
 
 function filter(arr) {
- 
+
   return arr.filter(name => name[0] === 'R');
-   
+
 }
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
@@ -65,17 +65,62 @@ const filteredNames = filter(myNames);
 //   // This is known as a "predicate function" - it's a function that 
 //   // only returns a boolean
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
-function hazardWarningCreator(typeOfWarning){
+function hazardWarningCreator(typeOfWarning) {
   let warningCounter = 0;
-  return function(location){
+  return function (location) {
     warningCounter++;
+    let times = 'times';
+    if (warningCounter === 1) {
+      times = 'time';
+    }
     console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-    console.log(`he ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-  }
+    console.log(`he ${typeOfWarning} hazard alert has triggered ${warningCounter} ${times} today!`);
+  };
 }
 const rocksWarning = hazardWarningCreator('Rocks on the Road');
 const floodWarning = hazardWarningCreator('Excesive flooding');
 const treeWarning = hazardWarningCreator('Fallen tree');
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+console.log(rocksWarning);
+
+function turtle(arr) {
+  return arr.filter(array => array[0] >= 0 && array[1] >= 0);
+}
+const newArr = turtle([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]);
+function movementS(arr) {
+  let stLeft = arr.map(function (array) {
+    return array[0] + array[1];
+  });
+  stLeft.forEach(num => console.log(num));
+}
+movementS(newArr);
+
+
+
+
+console.log(newArr);
+
+
+function decoder(str) {
+  let codedArr = str.split(' ');
+  console.log(codedArr);
+
+  let answer = '';
+
+  let changedArr = codedArr.map(function(str) {
+    if (str.length === 3) {
+      return ' ';
+    } else {
+      return str[str.length - 1].toUpperCase();
+    }
+  });
+  console.log(changedArr);
+  
+  console.log(changedArr.reduce((answer, currentValue = '') => answer + currentValue));
+}
+
+decoder('noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest');
