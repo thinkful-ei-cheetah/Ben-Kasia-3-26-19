9be360d0ecfb15682bbb6dece4557ee781ab22c2
Kasia <kasia@miroartdesign.com>
2019-03-26 16:40:50 -0400

    final

--- exercises.js
+++ exercises.js
@@ -65,17 +65,42 @@ const filteredNames = filter(myNames);
 //   // This is known as a "predicate function" - it's a function that 
 //   // only returns a boolean
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 function hazardWarningCreator(typeOfWarning){
   let warningCounter = 0;
   return function(location){
     warningCounter++;
+    let times = 'times';
+    if (warningCounter === 1){
+      times = 'time';
+    }
     console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-    console.log(`he ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-  }
+    console.log(`he ${typeOfWarning} hazard alert has triggered ${warningCounter} ${times} today!`);
+  };
 }
 const rocksWarning = hazardWarningCreator('Rocks on the Road');
 const floodWarning = hazardWarningCreator('Excesive flooding');
 const treeWarning = hazardWarningCreator('Fallen tree');
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+console.log(rocksWarning);
+
+function turtle(arr){
+  return arr.filter(array => array[0] >= 0 && array[1]>= 0);
+}
+const newArr = turtle([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]);
+function movementS(arr){
+  let stLeft= arr.map(function(array){
+    return array[0] + array[1];
+  });
+  stLeft.forEach(num => console.log(num));
+}
+movementS(newArr);
+
+
+
+
+
+console.log(newArr);
